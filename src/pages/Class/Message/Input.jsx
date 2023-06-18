6de01b736687a0112067f126f7ce3bcc78fbaa7c
Ankit Choudhary <sign.ankit14@gmail.com>
2023-06-18 21:41:34 +0530

    Fixed error in Push Group Chat

--- src/pages/Class/Message/Input.jsx
+++ src/pages/Class/Message/Input.jsx
@@ -12,164 +12,186 @@ import avatar from "../../../assets/img/placeholder_avatar.png";
 import { toast } from "react-toastify";
 
 export default function Input({ data }) {
-    const { address } = useAccount();
+	const { address } = useAccount();
 
-    // const { data: EnsNameData } = useEnsName({
-    //     address: address,
-    // });
+	// const { data: EnsNameData } = useEnsName({
+	//     address: address,
+	// });
 
-    // const { data: EnsAvatarData } = useEnsAvatar({
-    //     name: EnsNameData,
-    // });
+	// const { data: EnsAvatarData } = useEnsAvatar({
+	//     name: EnsNameData,
+	// });
 
-    const [loadMsg, setLoadMsg] = useState(null);
-    const [alreadyExists, setAlreadyExists] = useState(false);
+	const [loadMsg, setLoadMsg] = useState(null);
+	const [alreadyExists, setAlreadyExists] = useState(false);
 
-    const [textInp, setTextInp] = useState("");
+	const [textInp, setTextInp] = useState("");
 
-    const sendMessage = async () => {
-        setLoadMsg("Loading...");
-        try {
-            const { ethereum } = window;
+	const sendMessage = async () => {
+		setLoadMsg("Loading...");
+		try {
+			const { ethereum } = window;
 
-            if (ethereum) {
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner();
-                const accounts = await ethereum.request({
-                    method: "eth_requestAccounts",
-                });
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const accounts = await ethereum.request({
+					method: "eth_requestAccounts",
+				});
 
-                const message = data.message;
+				const message = data.message;
 
-                const startMarker = "**###@@";
-                const endMarker = "####*@@";
+				const startMarker = "****###@@";
+				const endMarker = "####***@@";
 
-                const startIndex = message.indexOf(startMarker) + startMarker.length;
-                const endIndex = message.indexOf(endMarker);
+				const startIndex = message.indexOf(startMarker) + startMarker.length;
+				const endIndex = message.indexOf(endMarker);
 
-                const chatId = message.substring(startIndex, endIndex);
+				const chatId = message.substring(startIndex, endIndex);
 
-                const pvtKeyStartIndex = endIndex + endMarker.length;
-                const pvtKey = message.substring(pvtKeyStartIndex);
+				const pvtKeyStartIndex = endIndex + endMarker.length;
+				const pvtKey = message.substring(pvtKeyStartIndex);
 
-                const user = await PushAPI.user.get({
-                    account: `eip155:${accounts[0]}`,
-                    env: "staging",
-                });
+				const user = await PushAPI.user.get({
+					account: `eip155:${accounts[0]}`,
+					env: "staging",
+				});
 
-                // need to decrypt the encryptedPvtKey to pass in the api using helper function
-                const pgpDecryptedPvtKey = await PushAPI.chat.decryptPGPKey({
-                    encryptedPGPPrivateKey: user.encryptedPrivateKey,
-                    signer: signer,
-                });
-                const groupDetails = await PushAPI.chat.getGroup({
-                    chatId: `${chatId}`,
-                    env: "staging",
-                });
+				// need to decrypt the encryptedPvtKey to pass in the api using helper function
+				const pgpDecryptedPvtKey = await PushAPI.chat.decryptPGPKey({
+					encryptedPGPPrivateKey: user.encryptedPrivateKey,
+					signer: signer,
+				});
+				const groupDetails = await PushAPI.chat.getGroup({
+					chatId: `${chatId}`,
+					env: "staging",
+				});
 
-                const arrayOfPreviousMembers = await Promise.all(
-                    groupDetails.members.map((name, index) => {
-                        const str = groupDetails.members[index].wallet.slice(7, 49);
-                        if (accounts[0] == str) {
-                            setAlreadyExists(true);
-                        }
-                        return str;
-                    })
-                );
+				const arrayOfPreviousMembers = await Promise.all(
+					groupDetails.members.map((name, index) => {
+						const str = groupDetails.members[index].wallet.slice(7, 49);
+						if (accounts[0] == str) {
+							setAlreadyExists(true);
+						}
+						return str;
+					})
+				);
 
-                console.log(arrayOfPreviousMembers);
-                try {
-                    await PushAPI.chat.updateGroup({
-                        chatId: `${chatId}`,
-                        groupName: `${groupDetails.groupName}`,
-                        groupDescription: `${groupDetails.groupDescription}`,
-                        members: [...arrayOfPreviousMembers, `${accounts[0]}`],
-                        groupImage: `${groupDetails.groupImage}`,
-                        admins: [`${groupDetails.groupCreator}`, `${accounts[0]}`],
-                        isPublic: true,
-                        account: `${groupDetails.groupCreator}`,
-                        env: "staging",
-                        pgpPrivateKey: pvtKey, //decrypted private key
-                    });
+				console.log(arrayOfPreviousMembers);
+				// try {
+				//     await PushAPI.chat.updateGroup({
+				//         chatId: `${chatId}`,
+				//         groupName: `${groupDetails.groupName}`,
+				//         groupDescription: `${groupDetails.groupDescription}`,
+				//         members: [...arrayOfPreviousMembers, `${accounts[0]}`],
+				//         groupImage: `${groupDetails.groupImage}`,
+				//         admins: [`${groupDetails.groupCreator}`, `${accounts[0]}`],
+				//         isPublic: true,
+				//         account: `${groupDetails.groupCreator}`,
+				//         env: "staging",
+				//         pgpPrivateKey: pvtKey, //decrypted private key
+				//     });
 
-                    await PushAPI.chat.approve({
-                        status: "Approved",
-                        account: `${accounts[0]}`,
-                        senderAddress: `${chatId}`, // receiver's address or chatId of a group
-                        env: "staging",
-                    });
-                } catch (error) { }
+				//     await PushAPI.chat.approve({
+				//         status: "Approved",
+				//         account: `${accounts[0]}`,
+				//         senderAddress: `${chatId}`, // receiver's address or chatId of a group
+				//         env: "staging",
+				//     });
+				// } catch (error) { }
 
-                setLoadMsg("Sending Message...");
+				if (!alreadyExists) {
+					await PushAPI.chat.updateGroup({
+						chatId: `${chatId}`,
+						groupName: `${groupDetails.groupName}`,
+						groupDescription: `${groupDetails.groupDescription}`,
+						members: [...arrayOfPreviousMembers, `${accounts[0]}`],
+						groupImage: `${groupDetails.groupImage}`,
+						admins: [`${groupDetails.groupCreator}`, `${accounts[0]}`],
+						isPublic: true,
+						account: `${groupDetails.groupCreator}`,
+						env: "staging",
+						pgpPrivateKey: pvtKey, //decrypted private key
+					});
 
-                // actual api
-                await PushAPI.chat.send({
-                    messageContent: `${textInp}`,
-                    messageType: "Text",
-                    receiverAddress: `${chatId}`,
-                    signer: signer,
-                    pgpPrivateKey: pgpDecryptedPvtKey,
-                    env: "staging",
-                });
+					await PushAPI.chat.approve({
+						status: "Approved",
+						account: `${accounts[0]}`,
+						senderAddress: `${chatId}`, // receiver's address or chatId of a group
+						env: "staging",
+					});
+				}
 
-                setLoadMsg(null);
-                toast.success("Message was sent to the class successfully!", {
-                    position: "top-center",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-            } else {
-                console.log("Ethereum object doesn't exist!");
-                setLoadMsg(null);
-                toast.error("Some problem with Metamask! Please try again", {
-                    position: "top-center",
-                    autoClose: 3000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-            }
-        } catch (error) {
-            console.log(error);
-            setLoadMsg(null);
-            toast.error("Some error was encountered while sending the message !", {
-                position: "top-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-            });
-        }
-    };
+				setLoadMsg("Sending Message...");
 
-    return (
-        <Container>
-            {/* <img src={EnsAvatarData ? EnsAvatarData : avatar} alt="" /> */}
-            <img src={avatar} alt="" />
-            <div>
-                <textarea
-                    type="text"
-                    placeholder="Announce something to your class"
-                    value={textInp}
-                    onChange={(e) => setTextInp(e.target.value)}
-                />
-                <Button style={{ padding: "7px 20px" }} onClick={sendMessage}>
-                    {loadMsg ? loadMsg : "Submit"}
-                </Button>
-            </div>
-        </Container>
-    );
+				// actual api
+				await PushAPI.chat.send({
+					messageContent: `${textInp}`,
+					messageType: "Text",
+					receiverAddress: `${chatId}`,
+					signer: signer,
+					pgpPrivateKey: pgpDecryptedPvtKey,
+					env: "staging",
+				});
+
+				setLoadMsg(null);
+				toast.success("Message was sent to the class successfully!", {
+					position: "top-center",
+					autoClose: 3000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			} else {
+				console.log("Ethereum object doesn't exist!");
+				setLoadMsg(null);
+				toast.error("Some problem with Metamask! Please try again", {
+					position: "top-center",
+					autoClose: 3000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "dark",
+				});
+			}
+		} catch (error) {
+			console.log(error);
+			setLoadMsg(null);
+			toast.error("Some error was encountered while sending the message !", {
+				position: "top-center",
+				autoClose: 3000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				theme: "dark",
+			});
+		}
+	};
+
+	return (
+		<Container>
+			{/* <img src={EnsAvatarData ? EnsAvatarData : avatar} alt="" /> */}
+			<img src={avatar} alt="" />
+			<div>
+				<textarea
+					type="text"
+					placeholder="Announce something to your class"
+					value={textInp}
+					onChange={(e) => setTextInp(e.target.value)}
+				/>
+				<Button style={{ padding: "7px 20px" }} onClick={sendMessage}>
+					{loadMsg ? loadMsg : "Submit"}
+				</Button>
+			</div>
+		</Container>
+	);
 }
 
 const Container = styled.div`
